03bde8f4f777b5cfa5f54cb274b0c52eeffa9bd4
jcblanc2 <jclaytonblanc@gmail.com>
2023-06-08 10:49:10 -0400

    CleanUp, make Repository class generic

---
+++ MagicVilla_API/Repository/IRepository/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using MagicVilla_API.Model;
+using System.Linq.Expressions;
+
+namespace MagicVilla_API.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        public Task<List<T>> GetAllAsync(Expression<Func<T, bool>>? filter = null);
+        public Task<T> GetAsync(Expression<Func<T, bool>> filter = null, bool tracked = true);
+        public Task CreateVillaAsync(T entity);
+        public Task RemoveAsync(T entity);
+        public Task SaveAsync();
+    }
+}
---
+++ MagicVilla_API/Repository/IRepository/IVillaRepository.cs
@@ -0,0 +1,11 @@
+﻿using MagicVilla_API.Model;
+using MagicVilla_API.Models.DTOS;
+using System.Linq.Expressions;
+
+namespace MagicVilla_API.Repository.IRepository
+{
+    public interface IVillaRepository : IRepository<Villa>
+    {
+        public Task UpdateAsync(Villa entity);
+    }
+}
