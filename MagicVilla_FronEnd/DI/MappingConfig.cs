5ccff34a56854657ed25fe54c5e541c2dd0785b8
jcblanc2 <jclaytonblanc@gmail.com>
2023-06-09 19:27:40 -0400

    add automapper

---
+++ MagicVilla_FronEnd/DI/MappingConfig.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using MagicVilla_FronEnd.Models.DTOS;
+
+namespace MagicVilla_FronEnd.DI
+{
+    public class MappingConfig : Profile
+    {
+        public MappingConfig() 
+        {
+            CreateMap<VillaDto, VillaCreateDto>().ReverseMap();
+            CreateMap<VillaDto, VillaUpdateDto>().ReverseMap();
+
+            CreateMap<VillaNumberDto, VillaNumberCreateDto>().ReverseMap();
+            CreateMap<VillaNumberDto, VillaNumberUpdateDto>().ReverseMap();
+        }
+    }
+}
---
+++ MagicVilla_FronEnd/Models/DTOS/VillaNumberCreateDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MagicVilla_FronEnd.Models.DTOS
+{
+    public class VillaNumberCreateDto
+    {
+
+        [Required]
+        public int VillaNo { get; set; }
+        [Required]
+        public int VillaID { get; set; }
+        public string SpecialDetails { get; set; }
+    }
+}
